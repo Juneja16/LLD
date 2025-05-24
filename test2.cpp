816570a624f7df614cefb04b1dc430e696f01d8c
Juneja16 <rjhk6767@gmail.com>
2025-05-24 21:04:15 +0530

    Playing with Git

---
+++ test2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout<<"Hello, World!"<<endl;
+    cout<<"This is a simple C++ program."<<endl;
+    cout<<"This program is designed to demonstrate basic C++ syntax."<<endl;
+    cout<<"It includes input/output operations and basic structure."<<endl;
+    cout<<"Feel free to modify and experiment with the code!"<<endl;
+    cout<<"Happy coding!"<<endl;
+    cout<<"End of the program."<<endl;
+    return 0;
+}
